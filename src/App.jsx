15c9d66e7428cc0677a5e194f2999143cfc7df70
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2025-10-10 07:06:11 -0700

    Merge; Merged content from updated main to prep Navbar for push to main.

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,15 @@ import PlanCard from "./components/PlanCard";
 import PlanShareModal from "./components/PlanShareModal";
 import Input from "./components/Input";
 import InputCard from "./components/InputCard";
-<<<<<<< HEAD
 import Navbar from "./components/Navbar";
-=======
 import DayPlanCard from "./components/DayPlanCard";
 import DayPlanCardEvent from "./components/DayPlanCardEvent";
 import DetailsItem from "./components/DetailsItem";
->>>>>>> 0d9d98aaae75e2610b3f4ba522b41f3ad67f0bed
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar navTitle="Navbar" />
-=======
->>>>>>> 0d9d98aaae75e2610b3f4ba522b41f3ad67f0bed
       <Button redSmall>Delete</Button>
       <Button redLarge>Delete</Button>
       <Button whiteSmall>Cancel</Button>
@@ -43,19 +37,47 @@ function App() {
         showAddButton={false}
         style={{ "--input-label-size": "20px" }}
       >
-<<<<<<< HEAD
         <Input title="Date" type="date" />
         <Input title="Destination" type="text" />
         <Input title="Budget in currency" type="number" showDollar={true} />
         <Input title="Members" />
       </InputCard>
-=======
+    </div>
+  );
+  return (
+    <div>
+      <Button redSmall>Delete</Button>
+      <Button redLarge>Delete</Button>
+      <Button whiteSmall>Cancel</Button>
+      <Button blueLarge>Save</Button>
+      <Button whiteLarge>Cancel</Button>
+      <CreatePlanButton />
+      <DeleteModal />
+      <PlanCard planName="Travel plan" planDate="2025-01-01" />
+      <PlanShareModal />
+      <Input title="Tour Name" />
+      <Input title="Date" type="date" />
+      <InputCard cardTitle="Day 1" style={{ "--input-label-size": "20px" }}>
+        <Input title="Location 01" />
+        <Input title="Date" type="date" />
+        <Input title="Time" type="time" />
+        <Input title="Note" />
+      </InputCard>
+      <InputCard
+        cardTitle="Our 1st Korea Trip"
+        showAddButton={false}
+        style={{ "--input-label-size": "20px" }}
+      >
         <DetailsItem title="Date" content="2025/10/04 - 2025/10/14" />
         <DetailsItem title="Destination" content="South Korea" />
         <DetailsItem title="Budget in currency" content="$4000" />
         <DetailsItem title="Members" content="wende, jisoo, yejin, sam" />
       </InputCard>
-      <DayPlanCard cardTitle="Day 01 - Oct 3rd, 4th" weatherIcon="☀️" weather="Sunny 16°C / 11°C">
+      <DayPlanCard
+        cardTitle="Day 01 - Oct 3rd, 4th"
+        weatherIcon="☀️"
+        weather="Sunny 16°C / 11°C"
+      >
         <DayPlanCardEvent
           date="2025/10/03"
           time="7:20"
@@ -63,7 +85,12 @@ function App() {
           event="Vancouver airport"
           note="Double-check visa!"
         />
-        <DayPlanCardEvent date="2025/10/04" time="18:00" timeZone="KST" event="Incheon airport" />
+        <DayPlanCardEvent
+          date="2025/10/04"
+          time="18:00"
+          timeZone="KST"
+          event="Incheon airport"
+        />
         <DayPlanCardEvent
           date="2025/10/04"
           time="20:00"
@@ -72,7 +99,11 @@ function App() {
           note="Lotte Hotel in Hongdae"
         />
       </DayPlanCard>
-      <DayPlanCard cardTitle="Day 02 - Oct 5th" weatherIcon="⛅️" weather="Cloudy 18°C / 9°C">
+      <DayPlanCard
+        cardTitle="Day 02 - Oct 5th"
+        weatherIcon="⛅️"
+        weather="Cloudy 18°C / 9°C"
+      >
         <DayPlanCardEvent
           time="8:00"
           timeZone="KST"
@@ -98,7 +129,6 @@ function App() {
           note="Not a museum, bagel cafe"
         />
       </DayPlanCard>
->>>>>>> 0d9d98aaae75e2610b3f4ba522b41f3ad67f0bed
     </div>
   );
 }
--- src/components/InputCard.jsx
+++ src/components/InputCard.jsx
@@ -1,16 +1,26 @@
 import styles from "./InputCard.module.css";
 import addIcon from "../assets/add-day-plus-btn.svg";
 
-<<<<<<< HEAD
 export default function InputCard({
   cardTitle,
   children,
   showAddButton = true,
   style,
 }) {
-=======
+  return (
+    <div className={styles.inputCardContainer} style={style}>
+      <div>
+        <p className={styles.cardTitle}>{cardTitle}</p>
+        {children}
+      </div>
+      {showAddButton && (
+        <button className={styles.addButton}>
+          <img src={addIcon} alt="add-day-plus-btn" />
+        </button>
+      )}
+    </div>
+  );
 export default function InputCard({ cardTitle, children, showAddButton = true, style }) {
->>>>>>> 0d9d98aaae75e2610b3f4ba522b41f3ad67f0bed
   return (
     <div className={styles.inputCardContainer} style={style}>
       <div>
