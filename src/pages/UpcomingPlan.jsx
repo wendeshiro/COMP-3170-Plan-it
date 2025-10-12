78d50751c8fd60b5e13fdf6d9b02e6ded8767fe6
j1s00jang <jjang87@my.bcit.ca>
2025-10-12 00:32:18 -0700

    Upcoming Plan Static page without dropdown

---
+++ src/pages/UpcomingPlan.jsx
@@ -0,0 +1,42 @@
+import "./UpcomingPlan.css";
+import logo from "../assets/plan-it_logo.png";
+import PlanCard from "../components/PlanCard";
+import AddPlan from "../components/AddPlan";
+
+const UpcomingPlan = () => {
+  return (
+    <div className="upcoming-plan">
+      <div className="plan-it-logo">
+        <img
+          src={logo}
+          alt="Plan It Logo"
+        />
+      </div>
+
+      <div className="dropdown-menu"></div>
+
+      <div className="select-btn-container">
+        <h3 className="select-btn-text">Select</h3>
+      </div>
+
+      <div className="plan-cards-container">
+        <div className="plan-cards">
+          <PlanCard
+            planName="Our 1st Korea Trip"
+            planDate="2025-10-05"
+          />
+
+          <PlanCard
+            planName="China Family Trip"
+            planDate="2025-12-08"
+          />
+        </div>
+      </div>
+
+      <div className="add-plan-button"></div>
+      <AddPlan />
+    </div>
+  );
+};
+
+export default UpcomingPlan;
