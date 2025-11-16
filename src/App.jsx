dbc0e4c409e8fc85c93245bce9ab971dccd66568
wendeshiro <shiro12311@gmail.com>
2025-11-16 03:44:09 -0800

    Fix; remove route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
 import UpcomingPlan from "./pages/UpcomingPlan";
-import CreatePlanPage from "./pages/CreatePlanPage";
-import PlanDetail from "./pages/PlanDetail";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<UpcomingPlan />} />
-        <Route path="/create" element={<CreatePlanPage />} />
-        <Route path="/detail" element={<PlanDetail />} />
-      </Routes>
+      <UpcomingPlan />
     </div>
   );
 }
